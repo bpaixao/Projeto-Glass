ecea41f7671809835cd6aad2241fd17ee1d0f320
Beatriz Paixão <bpaixao@live.com>
2020-11-08 19:18:11 -0300

    calc-update-arqjs

--- projeto-glass-html5/_javascript/funcoes.js
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,11 @@
 function mudaFoto(foto){
     document.getElementById("icone").src = foto;
 }
+
+
+/*Calculo do valor do Glass - no Form de Cadastro*/
+function calc_total() {
+    var qtd = document.getElementById('cQtd').value;
+    qtd = qtd * 1500;
+    document.getElementById('cTot').value = qtd;
+}
